dd734336cd596e360945ab3ba23f27f1ded3231d
kwongio <rldh11111@naver.com>
2024-07-11 21:19:40 +0900

    initial?

---
+++ src/main/java/org/example/threadsafe/ThreadSafeApplication.java
@@ -0,0 +1,13 @@
+package org.example.threadsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadSafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadSafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/threadsafe/ThreadSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.threadsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadSafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
